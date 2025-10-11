3d5d2777eed1ba6d703dcd531a9893bfeee1a574
sawan jha <jhasawan02@gmail.com>
2025-10-11 11:10:53 +0530

    task1 and task2 completed

---
+++ Task.js
@@ -0,0 +1,32 @@
+//Task1.
+// let name = prompt("What is your name? ");
+// let number = prompt("What is your favourite number? ");
+// console.log(`hello ${name},are you ${number} years old?`);
+
+//Task2.
+// let number1 = Number(prompt("Enter the number: "));
+// let operator = prompt("Enter the operator(+ - * /): ");
+// let number2 = Number(prompt("Enter the another number: "));
+// let result = 0;
+
+// // switch(operator){
+// //     case "+": result = number1+number2;
+// //     break;
+// //     case "-": result = number1-number2;
+// //     break;
+// //     case "*": result = number1 * number2;
+// //     break;
+// //     case "/": result = number1 / number2;
+// //     break;
+// //     default: result ="invalid opeartor";
+// // }
+
+// // if(operator === "+"){result = number1 + number2}; 
+// // if(operator === "-"){result = number1 - number2};
+// // if(operator === "*"){result = number1 * number2};
+// // if(operator === "/"){result = number1 / number2};
+
+// if(result % 2 !== 0){console.log("We don't give odd number in our result!");}
+// else{console.log(`Result: ${result}`);}
+
+//Task3.
